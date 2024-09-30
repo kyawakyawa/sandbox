37a849f502170fca9b338ab9b2b6ea19694512a0
kyawakyawa <kyawashell@gmail.com>
2024-09-30 17:54:49 +0900

    update

--- python/apply_sim3/lib/create_case.py
+++ python/apply_sim3/lib/create_case.py
@@ -2,16 +2,20 @@ import numpy as np
 
 
 def create_simple_case():
-    cam_a_cen_world = np.array([[0, 0, 0], [1, 0, 0], [1, -1, 0], [0, -1, 0]])
-    cam_b_cen_world = np.array([[0, -1, 1], [0, -1, 3], [0, -3, 3], [0, -3, 1]])
+    cam_a_cen_world = np.array(
+        [[0, 0, 0], [1, 0, 0], [1, -1, 0], [0, -1, 0]], dtype=np.float64
+    )
+    cam_b_cen_world = np.array(
+        [[0, -1, 1], [0, -1, 3], [0, -3, 3], [0, -3, 1]], dtype=np.float64
+    )
 
     # rotations are common for all cameras
-    cam_a_rot = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]])[None, :, :].repeat(
-        4, axis=0
-    )
-    cam_b_rot = np.array([[0, -1, 0], [0, 0, -1], [1, 0, 0]])[None, :, :].repeat(
-        4, axis=0
-    )
+    cam_a_rot = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, 1]], dtype=np.float64)[
+        None, :, :
+    ].repeat(4, axis=0)
+    cam_b_rot = np.array([[0, 0, -1], [0, -1, 0], [-1, 0, 0]], dtype=np.float64)[
+        None, :, :
+    ].repeat(4, axis=0)
 
     return cam_a_rot, cam_a_cen_world, cam_b_rot, cam_b_cen_world
 
@@ -23,8 +27,12 @@ def create_simple_points(n: int, scale: float, rot: np.ndarray, trans: np.ndarra
     # 0.5 < z < 1.5
     # で点をn個作成
     points = np.random.rand(n, 3)
-    points_applied_sim3 = (scale * rot @ points[:, None] + trans[:, None]).squeeze(
+    points[:, 0] = points[:, 0]
+    points[:, 1] = -points[:, 1]
+    points[:, 2] = points[:, 2] + 0.5
+
+    points_applied_sim3 = (scale * rot[None] @ points[:, :, None]).squeeze(
         axis=-1
-    )
+    ) + trans[None]
 
     return points, points_applied_sim3
--- python/apply_sim3/main.py
+++ python/apply_sim3/main.py
@@ -3,8 +3,11 @@ import argparse
 import lib.align
 import lib.create_case
 import numpy as np
+import pypose
 import rerun as rr  # pip install rerun-sdk
 import rerun.blueprint as rrb
+import torch
+from pypose import mat2SO3
 
 
 def tomat44(scale: float, rot: np.ndarray, trans: np.ndarray):
@@ -20,17 +23,52 @@ def tomat44(scale: float, rot: np.ndarray, trans: np.ndarray):
     return ret
 
 
-def draw(cam_rot: np.ndarray, cam_cen_world: np.ndarray):
+def draw(cam_rot: np.ndarray, cam_cen_world: np.ndarray, points: np.ndarray):
     """
     cam_rot: n x 3 x 3
     cam_cen_world: n x 3
     """
 
+    n = cam_rot.shape[0]
     # rotation of world to local
     rot_l2w = cam_rot.transpose(0, 2, 1)
-
     # translation of world to local
     trans_l2w = (-rot_l2w @ cam_cen_world[:, :, None]).squeeze(axis=-1)
+
+    for i in range(n):
+        txyz = trans_l2w[i]
+        rot_l2w_tensor = torch.from_numpy(rot_l2w[i])
+
+        qxyzw = mat2SO3(rot_l2w_tensor).data.cpu().numpy()
+
+        bp_path = f"/camera{i:04d}"
+
+        rr.log(
+            bp_path,
+            rr.Transform3D(
+                translation=txyz,
+                rotation=rr.Quaternion(xyzw=qxyzw),
+                from_parent=True,
+            ),
+        )
+        rr.log(
+            bp_path, rr.ViewCoordinates.RDF, static=True
+        )  # X=Right, Y=Down, Z=Forward
+
+        rr.log(
+            bp_path + "/image",
+            rr.Pinhole(
+                resolution=[960, 512],
+                focal_length=[960 / 6, 960 / 6],
+                principal_point=[480, 256],
+                image_plane_distance=0.1,
+            ),
+        )
+    point_colors = np.full((points.shape[0], 3), 255, dtype=np.uint8)
+    rr.log(
+        "points",
+        rr.Points3D(points, colors=point_colors),
+    )
 
 
 def main():
@@ -44,6 +82,8 @@ def main():
 
     rr.script_setup(args, "appling_sim3_test", default_blueprint=blueprint)
 
+    rr.log("/", rr.ViewCoordinates.RIGHT_HAND_Y_DOWN, static=True)
+
     cam_a_rot, cam_a_cen_world, cam_b_rot, cam_b_cen_world = (
         lib.create_case.create_simple_case()
     )
@@ -54,6 +94,12 @@ def main():
     print("scale: ", scale)
 
     points_a, points_b = lib.create_case.create_simple_points(100, scale, rot, trans)
+
+    rr.set_time_seconds("stable_time", 1)
+    draw(cam_a_rot, cam_a_cen_world, points_a)
+
+    rr.set_time_seconds("stable_time", 2)
+    draw(cam_b_rot, cam_b_cen_world, points_b)
 
     rr.script_teardown(args)
 
