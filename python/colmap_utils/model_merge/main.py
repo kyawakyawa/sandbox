a02d48b3b96b3603c5f2f1fb45ee9c1ac1cc3b4a
kyawakyawa <kyawashell@gmail.com>
2024-10-19 17:31:13 +0900

    model_merge

---
+++ python/colmap_utils/model_merge/main.py
@@ -0,0 +1,135 @@
+from dataclasses import dataclass
+from pathlib import Path
+import os
+import copy
+
+import tyro
+from rich.console import Console
+from read_write_model import (
+    read_model,
+    Camera,
+    Image,
+    write_cameras_text,
+    write_images_text,
+)
+
+console = Console()
+CameraDict = dict[int, Camera]
+ImageDict = dict[int, Image]
+
+
+@dataclass
+class Config:
+    models: list[str]
+    output_path: str
+    single_camera: bool = True
+
+
+@dataclass
+class Model:
+    cameras: CameraDict
+    images: ImageDict
+
+
+def load_models(model_names: list[str]) -> dict[str, Model]:
+    models: dict[str, Model] = {}
+    for model in model_names:
+        exist_bin = os.path.exists(os.path.join(model, "images.bin"))
+        exist_txt = os.path.exists(os.path.join(model, "images.txt"))
+        assert exist_bin or exist_txt, f"images.bin or images.txt not found in {model}"
+
+        cameras: CameraDict = {}
+        images: ImageDict = {}
+        if exist_bin:
+            cameras, images, _ = read_model(Path(model), ext=".bin")  # pyright: ignore
+        else:
+            cameras, images, _ = read_model(Path(model), ext=".txt")  # pyright: ignore
+
+        assert type(cameras) is dict, "cameras should be a dict"
+        assert type(images) is dict, "images should be a dict"
+
+        models[model] = Model(cameras=cameras, images=images)
+
+    return models
+
+
+def merge_models(models: dict[str, Model], single_camera: bool) -> Model:
+    merged_cameras: CameraDict = {}
+    merged_images: ImageDict = {}
+
+    image_id_offset = 0
+    camera_id_offset = 0
+    if single_camera:
+        cameras = next(iter(models.values())).cameras
+        camera = next(iter(cameras.values()))
+        merged_cameras[1] = Camera(
+            id=1,
+            model=camera.model,
+            width=camera.width,
+            height=camera.height,
+            params=camera.params,
+        )
+
+    for model in models.values():
+        cameras = model.cameras
+        images = model.images
+
+        if not single_camera:
+            for camera_id, camera in cameras.items():
+                new_camera_id = camera_id + camera_id_offset
+                new_camera = Camera(
+                    id=new_camera_id,
+                    model=camera.model,
+                    width=camera.width,
+                    height=camera.height,
+                    params=camera.params,
+                )
+                assert new_camera_id not in merged_cameras
+                merged_cameras[new_camera_id] = new_camera
+
+        for image_id, image in images.items():
+            new_image = copy.deepcopy(image)
+            new_camera_id = (
+                image.camera_id + camera_id_offset if not single_camera else 1
+            )
+
+            new_image_id = image_id + image_id_offset
+            new_image = Image(
+                id=new_image_id,
+                qvec=image.qvec,
+                tvec=image.tvec,
+                camera_id=new_camera_id,
+                name=image.name,
+                xys=[],
+                point3D_ids=[],
+            )
+            assert new_image_id not in merged_images
+            merged_images[new_image_id] = new_image
+
+        camera_id_offset += max(cameras.keys()) + 1
+        image_id_offset += max(images.keys()) + 1
+
+    return Model(cameras=merged_cameras, images=merged_images)
+
+
+def main(config: Config):
+    models = load_models(config.models)
+    merged_model = merge_models(models, config.single_camera)
+
+    os.makedirs(config.output_path, exist_ok=True)
+
+    write_cameras_text(
+        merged_model.cameras, os.path.join(config.output_path, "cameras.txt")
+    )
+    write_images_text(
+        merged_model.images, os.path.join(config.output_path, "images.txt")
+    )
+
+    with open(os.path.join(config.output_path, "points3D.txt"), "w") as f:
+        f.write("")
+
+
+if __name__ == "__main__":
+    config = tyro.cli(Config)
+    print(config)
+    main(config)
